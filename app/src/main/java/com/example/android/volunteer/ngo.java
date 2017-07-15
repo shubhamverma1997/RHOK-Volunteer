a75bcd2e2b330729da199ae54ecc178ab0ae119f
Shubham Verma <lifemeansmore008@gmail.com>
2017-07-15 17:41:25 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/volunteer/ngo.java
@@ -0,0 +1,71 @@
+package com.example.android.volunteer;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by shubham on 7/15/17.
+ */
+
+public class ngo {
+    private String mTitle;
+    private String mDescription;
+    private Bitmap mImage;
+    private String mNgoTitle;
+    private int mPeopleRequired;
+    private String[] mSkills;
+    private int[] mRequirement;
+    private long mEventDate;
+    private long mCreateDate;
+
+    public ngo(String title,String description,Bitmap image,String ngoTitle,int peopleRequired,String[] skills
+                ,int[] requirement,long eventDate,long createDate)
+    {
+        mTitle=title;
+        mDescription=description;
+        mImage=image;
+        mNgoTitle=ngoTitle;
+        mPeopleRequired=peopleRequired;
+        mSkills=skills;
+        mRequirement=requirement;
+        mEventDate=eventDate;
+        mCreateDate=createDate;
+    }
+
+    public String getTitle()
+    {
+        return mTitle;
+    }
+    public String getDescription()
+    {
+        return mDescription;
+    }
+    public Bitmap getImage()
+    {
+        return mImage;
+    }
+    public String getNgoTitle()
+    {
+        return mNgoTitle;
+    }
+    public int getPeopleRequired()
+    {
+        return mPeopleRequired;
+    }
+
+    public long getEventDate()
+    {
+        return mEventDate;
+    }
+    public long getCreateDate()
+    {
+        return mCreateDate;
+    }
+    public String[] getSkills()
+    {
+        return mSkills;
+    }
+    public int[] getRequirement()
+    {
+        return mRequirement;
+    }
+}
